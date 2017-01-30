65d71c82dbe725ca5f901b6e367d74007e64cafd
Sunday Nwuguru <sunday.nwuguru@andela.com>
2017-01-30 10:58:18 +0100

    List, add and remove items

--- src/main.js
+++ src/main.js
@@ -3,56 +3,162 @@
  */
 
 var React = require('react');
-
-
 import  {
     AppRegistry,
     StyleSheet,
     Text,
     View,
+    TextInput,
+    ListView,
     TouchableHighlight
 }  from 'react-native';
+var Firebase = require('firebase');
 
 var Todo = React.createClass({
-    getInitialState: () => {
+    getInitialState: function() {
+        var config = {
+            apiKey: "AIzaSyCnfsQd_Rq9TgVryG8O-pyESly3YbiO-B4",
+            authDomain: "mightys.firebaseapp.com",
+            databaseURL: "https://mightys.firebaseio.com",
+            storageBucket: "firebase-mightys.appspot.com",
+            messagingSenderId: "92071818184"
+        };
+        Firebase.initializeApp(config);
+
+        var db = Firebase.database();
+        this.itemsRef = db.ref('items');
+        this. items = [];
         return {
-            notes: []
+            newTodo: '',
+            todoSource: new ListView.DataSource({rowHasChanged: (row1, row2) => row1 !== row2})
         }
     },
-    render: () => {
+    componentDidMount: function() {
+      this.itemsRef.on('child_added', (dataSnapshot) => {
+        this.items.push({id: dataSnapshot.key, text: dataSnapshot.val()});
+        this.setState({
+          todoSource: this.state.todoSource.cloneWithRows(this.items)
+        });
+      });
+
+      this.itemsRef.on('child_removed', (dataSnapshot) => {
+          this.items = this.items.filter((x) => x.id !== dataSnapshot.key);
+          this.setState({
+            todoSource: this.state.todoSource.cloneWithRows(this.items)
+          });
+      });
+
+    },
+    render: function() {
         return (
-          <View style={styles.container}>
-            <Text style={styles.welcome}>
-              Welcome to React Native!
-            </Text>
-            <Text style={styles.instructions}>
-              To get started, edit index.android.js
-            </Text>
-            <Text style={styles.instructions}>
-              Double tap R on your keyboard to reload,{'\n'}
-              Shake or press menu button for dev menu
-            </Text>
-          </View>
+          <View style={styles.appContainer}>
+              <View style={styles.titleView}>
+                <Text style={styles.titleText}>
+                  My Todos
+                </Text>
+              </View>
+              <View style={styles.inputcontainer}>
+                <TextInput style={styles.input} onChangeText={(text) => this.setState({newTodo: text})} value={this.state.newTodo}/>
+                <TouchableHighlight
+                  style={styles.button}
+                  onPress={() => this.addTodo()}
+                  underlayColor='#dddddd'>
+                  <Text style={styles.btnText}>Add!</Text>
+                </TouchableHighlight>
+              </View>
+              <ListView
+                dataSource={this.state.todoSource}
+                renderRow={this.renderRow} />
+            </View>
         );
-     }
+     },
+    addTodo: function()  {
+      if (this.state.newTodo !== '') {
+        this.itemsRef.push({
+          todo: this.state.newTodo
+        });
+        this.setState({
+          newTodo : ''
+        });
+      }
+    },
+    removeTodo: function(rowData) {
+      this.itemsRef.child(rowData.id).remove();
+    },
+    renderRow: function(rowData) {
+      return (
+        <TouchableHighlight
+          underlayColor='#dddddd'
+          onPress={() => this.removeTodo(rowData)}>
+          <View>
+            <View style={styles.row}>
+              <Text style={styles.todoText}>{rowData.text.todo}</Text>
+            </View>
+            <View style={styles.separator} />
+          </View>
+        </TouchableHighlight>
+      );
+    }
 });
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer:{
+    flex: 1
+  },
+  titleView:{
+    backgroundColor: '#48afdb',
+    paddingTop: 30,
+    paddingBottom: 10,
+    flexDirection: 'row'
+  },
+  titleText:{
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: 'bold',
     flex: 1,
+    fontSize: 20
+  },
+  inputcontainer: {
+    marginTop: 5,
+    padding: 10,
+    flexDirection: 'row'
+  },
+  button: {
+    height: 36,
+    flex: 2,
+    flexDirection: 'row',
+    backgroundColor: '#48afdb',
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    color: 'white',
+    borderRadius: 4
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  btnText: {
+    fontSize: 18,
+    color: 'white',
+    marginTop: 6
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  input: {
+    height: 36,
+    padding: 4,
+    marginRight: 5,
+    flex: 4,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#48afdb',
+    borderRadius: 4,
+    color: '#48BBEC'
+  },
+  row: {
+    flexDirection: 'row',
+    padding: 12,
+    height: 44
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#CCCCCC'
+  },
+  todoText: {
+    flex: 1
   }
 });
 
